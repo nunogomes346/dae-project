1281612dd88c66bb326f6af1a44d016f19c31b1c
Nuno Gomes <2140624@my.ipleiria.pt>
2016-12-20 16:24:20 +0000

    complete separation of the 2 applications

--- HealthManagement/HealthManagement-war/src/java/web/AdministratorManager.java
+++ HealthManagement/HealthManagement-war/src/java/web/AdministratorManager.java
@@ -9,19 +9,11 @@ import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
 import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIParameter;
 import javax.faces.event.ActionEvent;
-import javax.inject.Inject;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 @ManagedBean
 @Named(value="administratorManager")
@@ -34,31 +26,17 @@ public class AdministratorManager implements Serializable {
     @EJB
     private HealthcareProBean healthcareProBean;
     
-    @Inject
-    private UserManager userManager;
-    
     private AdministratorDTO newAdministrator;
     private AdministratorDTO currentAdministrator;
     private HealthcareProDTO newHealthcarePro;
     private HealthcareProDTO currentHealthcarePro;
-    private HttpAuthenticationFeature feature;
     
-    private Client client;
-    private final String baseUri = "http://localhost:8080/HealthManagement-war/webapi";
     private UIComponent component;
     private static final Logger LOGGER = Logger.getLogger("web.AdministratorManager");
     
     public AdministratorManager() {
         newAdministrator = new AdministratorDTO();
         newHealthcarePro = new HealthcareProDTO();
-        client = ClientBuilder.newClient();        
-        feature = null;
-    }
-    
-    @PostConstruct
-    public void initClient() {
-        feature = HttpAuthenticationFeature.basic(userManager.getUsername(), userManager.getPassword());
-        client.register(feature);
     }
     
     // ***************************************
@@ -117,69 +95,6 @@ public class AdministratorManager implements Serializable {
             administratorBean.remove(id);
         } catch (Exception e) {
             LOGGER.warning("Error: problem in method removeAdministrator");
-        }
-    }
-    
-    public String createAdministratorREST() { 
-        try {
-            client.target(baseUri)
-                    .path("/administrators/create")
-                    .request(MediaType.APPLICATION_XML).post(Entity.xml(newAdministrator));
-            
-            newAdministrator.reset();
-            
-            return "admin_index?faces-redirect=true";
-        } catch (Exception e) {
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
-        }
-        return null;
-    }
-    
-    public List<AdministratorDTO> getAllAdministratorsREST() {
-        
-        
-        List<AdministratorDTO> returnedAdministrators = null;
-        try {
-            returnedAdministrators = client.target(baseUri)
-                .path("/administrators/all")
-                .request(MediaType.APPLICATION_XML)
-                .get(new GenericType<List<AdministratorDTO>>() {});
-        } catch (Exception e){
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
-        }
-        return returnedAdministrators;
-    }
-    
-    public String updateAdministratorREST() {   
-        try {
-            client.target(baseUri)
-                    .path("/administrators/update")
-                    .request(MediaType.APPLICATION_XML).put(Entity.xml(currentAdministrator));
-            
-            if(currentAdministrator.getUsername().compareTo(userManager.getUsername()) == 0) {
-                return userManager.logout();
-            }
-            
-            return "admin_index?faces-redirect=true";
-           
-        } catch (Exception e) {
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
-        }
-        return "admin_administrator_update";
-    }
-    
-    public void removeAdministratorREST(ActionEvent event) {
-        try {
-            UIParameter param = (UIParameter) event.getComponent().findComponent("deleteAdministratorId");
-            String id = param.getValue().toString();
-            
-            client.target(baseUri)
-                    .path("/administrators/delete/{username}")
-                    .resolveTemplate("username", id)
-                    .request().delete();
-
-        } catch (Exception e) {
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
         }
     }
 
@@ -241,75 +156,11 @@ public class AdministratorManager implements Serializable {
         } catch (Exception e) {
             LOGGER.warning("Error: problem in method removeHealthcarePro");
         }
-    }
-    
-    public String createHealthcareProREST() { 
-        try {
-            client.target(baseUri)
-                    .path("/healthcarepros/create")
-                    .request(MediaType.APPLICATION_XML).post(Entity.xml(newHealthcarePro));
-            
-            newHealthcarePro.reset();
-            
-            return "admin_index?faces-redirect=true";
-        } catch (Exception e) {
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
-        }
-        return null;
-    }
-    
-    public List<HealthcareProDTO> getAllHealthcareProsREST() {
-        List<HealthcareProDTO> returnedHealthcarePros = null;
-        try {
-            returnedHealthcarePros = client.target(baseUri)
-                .path("/healthcarepros/all")
-                .request(MediaType.APPLICATION_XML)
-                .get(new GenericType<List<HealthcareProDTO>>() {});
-        } catch (Exception e){
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
-        }
-        return returnedHealthcarePros;
-    }
-    
-    public String updateHealthcareProREST() { 
-        try {
-           client.target(baseUri)
-                    .path("/healthcarepros/update")
-                    .request(MediaType.APPLICATION_XML).put(Entity.xml(currentHealthcarePro));
-            return "admin_index?faces-redirect=true";
-           
-        } catch (Exception e) {
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
-        }
-        return "admin_healthcarepro_update";
-    }
-    
-    public void removeHealthcareProREST(ActionEvent event) {
-        try {
-            UIParameter param = (UIParameter) event.getComponent().findComponent("deleteHealthcareProId");
-            String id = param.getValue().toString();
-            
-            client.target(baseUri)
-                    .path("/healthcarepros/delete/{username}")
-                    .resolveTemplate("username", id)
-                    .request().delete();
-
-        } catch (Exception e) {
-            FacesExceptionHandler.handleException(e, "Unexpected error! Try again latter!", LOGGER);
-        }
-    }    
+    }  
     
     // **********************************
     // ************ GETS&SETS *******
-    // **********************************
-    public UserManager getUserManager() {
-        return userManager;
-    }
-
-    public void setUserManager(UserManager userManager) {
-        this.userManager = userManager;
-    }
-    
+    // **********************************    
     public AdministratorDTO getNewAdministrator() {
         return newAdministrator;
     }
